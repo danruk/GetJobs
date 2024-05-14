4c1781433c90e9d418e4bbcee148ff06b363097b
danruk <149634927+danruk@users.noreply.github.com>
2024-05-14 09:35:27 -0700

    completed the navigation bar for the app

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { View, ScrollView, SafeAreaView,Text} from 'react-native';
 import {Stack, useRouter} from 'expo-router';
 import {  COLORS, icons, images, SIZES} from '../constants';
-import { NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome} from '../components';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
 
 const Home = () => {
     const router= useRouter();
@@ -17,11 +17,26 @@ const Home = () => {
             < ScreenHeaderBtn iconUrl={icons.menu} dimension ="60%"/>
         ),
         headerRight: () => (
-            < ScreenHeaderBtn iconUrl={icons.menu} dimension ="100%"/>
+            < ScreenHeaderBtn iconUrl={images.menu} dimension ="100%"/>
         ),
         
        }}
        />
+       <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+        style={{
+            flex: 1,
+            padding: SIZES.medium
+        }}
+        >
+
+        <Welcome/>
+        <Popularjobs/>
+        <Nearbyjobs/>
+        </View>
+
+
+       </ScrollView>
     </SafeAreaView>
     )
 }
